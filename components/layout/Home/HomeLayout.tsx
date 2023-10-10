b0950d31344a34ec919507eb704a85451c603282
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2023-10-10 18:33:34 +0700

    :lipstick: style(app): responsive sidebar

--- components/layout/Home/HomeLayout.tsx
+++ components/layout/Home/HomeLayout.tsx
@@ -3,15 +3,26 @@ import * as React from 'react';
 import Header from '@/components/layout/Home/Header';
 import SideBar from '@/components/layout/Home/SideBar';
 
+import clsxm from '@/utils/lib/clsxm';
+
 interface LayoutsProps {
   children: React.ReactNode;
 }
 
 export default function HomeLayout({ children }: LayoutsProps) {
   return (
-    <div className='relative flex'>
-      <div className='container relative mx-auto flex w-full flex-row justify-between gap-4 px-20 py-4'>
-        <main className='hide-scrollbar h-[calc(100vh-32px)] flex-1 overflow-y-scroll pt-6'>
+    <div className='relative scroll-smooth transition-all duration-300'>
+      <div
+        className={clsxm(
+          'container relative mx-auto flex w-full flex-col-reverse justify-start gap-4 px-10 py-4',
+          'lg:flex-row lg:justify-between lg:px-20'
+        )}
+      >
+        <main
+          className={clsxm(
+            'hide-scrollbar w-full lg:h-[calc(100vh-32px)] lg:flex-1 lg:overflow-y-scroll lg:pt-6'
+          )}
+        >
           <div className='h-screen'>
             <Header />
             {children}
--- components/layout/Home/SideBar.tsx
+++ components/layout/Home/SideBar.tsx
@@ -1,59 +1,35 @@
-'use client';
-
-import { Icon } from '@iconify/react';
 import * as React from 'react';
 
-import {
-  Divider,
-  NextImageFill,
-  Status,
-  ToggleThemeIcon,
-} from '@/components/elements';
+import { Divider } from '@/components/elements';
+import MenuItem from '@/components/layout/Home/elements/MenuItem';
+import ProfilHeader from '@/components/layout/Home/elements/ProfilHeader';
 
-import { BackdropImage, ProfilImage } from '@/utils/constants/imageCloud';
+import clsxm from '@/utils/lib/clsxm';
 
-export default function SideBar() {
+const SideBar = () => {
   return (
-    <div className='hide-scrollbar sticky top-0 h-[calc(100vh-32px)] w-[300px] overflow-y-scroll px-6 pt-6'>
+    <div
+      className={clsxm(
+        'hide-scrollbar w-full',
+        'lg:sticky lg:top-0 lg:h-[calc(100vh-32px)]  lg:w-[300px] lg:overflow-y-scroll lg:px-6 lg:pt-6'
+      )}
+    >
       <div className='flex flex-col items-center justify-start gap-4'>
-        <div className='flex w-full flex-col items-center justify-between space-y-4'>
-          <div className='relative flex h-24 w-full items-center justify-center'>
-            <Status />
-            <NextImageFill
-              alt='backdrop-image'
-              src={BackdropImage}
-              figureClassName='h-full w-full rounded-lg overflow-hidden dark:brightness-50'
-              imageClassName='scale-125'
-            />
-            <NextImageFill
-              figureClassName='absolute z-10 border-2 border-white dark:border-neutral-800 aspect-square overflow-hidden h-24 translate-y-1/2 rounded-full bg-red-300 shadow-md bg-gray-200'
-              imageClassName='scale-105 hover:scale-110'
-              alt='profil-image'
-              src={ProfilImage}
-            />
-            <div className='absolute bottom-1 right-1 overflow-hidden rounded-lg'>
-              <ToggleThemeIcon />
-            </div>
-          </div>
-          <div className='flex flex-col items-center justify-center gap-1 pt-12'>
-            <div className='flex flex-row items-center justify-center gap-2'>
-              <h2 className='h4 text-primary-700'>M Teguh Irawan</h2>
-              <Icon
-                icon='ic:baseline-verified'
-                className='text-lg text-blue-500'
-              />
-            </div>
-
-            <span className='text-response-sm font-normal text-primary-600 dark:text-darkText'>
-              @onedev
-            </span>
-          </div>
-        </div>
+        <ProfilHeader />
         <nav className='w-full'>
           <Divider className='border-t-2' />
-          Nav
+          <div className='flex flex-col items-start justify-start gap-2'>
+            <MenuItem label='Home' />
+            <MenuItem label='Home' />
+            <MenuItem label='Home' />
+            <MenuItem label='Home' />
+            <MenuItem label='Home' />
+          </div>
+          <Divider className='border-t-2' />
         </nav>
       </div>
     </div>
   );
-}
+};
+
+export default SideBar;
