94ea4220c86de84e3c96a23d5f2298301343a816
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2023-10-12 22:12:23 +0700

    :sparkles: feat(app): fix

--- components/elements/index.ts
+++ components/elements/index.ts
@@ -3,7 +3,7 @@ import NextImageFill from '@/components/elements/global/NextImageFill';
 import Status from '@/components/elements/global/Status';
 import ToggleThemeIcon from '@/components/elements/global/ToggleThemeIcon';
 import PrimaryLink from '@/components/elements/links/PrimaryLink';
-import SectionWrapper from '@/components/elements/wrapper/sectionWrapper';
+import SectionWrapper from '@/components/elements/wrapper/SectionWrapper';
 
 export {
   Divider,
