ab041d4c1193cc2dbdba83c857e7fc0333b799ca
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2024-05-01 22:25:18 +0700

    :bug: fix(app): fix bug url meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : process.env.DOMAIN || ''
+      : process.env.NEXT_PUBLIC_URL || ''
   ),
   title: `${METADATA.creator} ${METADATA.exTitle}`,
   description: METADATA.description,
@@ -22,13 +22,13 @@ export const metadata: Metadata = {
     url: METADATA.openGraph.url,
   },
   icons: METADATA.profile,
-  openGraph: {
-    images: METADATA.profile,
-    url: METADATA.openGraph.url,
-    siteName: METADATA.openGraph.siteName,
-    locale: METADATA.openGraph.locale,
-    type: 'website',
-  },
+  // openGraph: {
+  //   images: METADATA.profile,
+  //   url: METADATA.openGraph.url,
+  //   siteName: METADATA.openGraph.siteName,
+  //   locale: METADATA.openGraph.locale,
+  //   type: 'website',
+  // },
 };
 
 export default function RootLayout({
