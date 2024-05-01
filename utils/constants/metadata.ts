b24a8252218276823e55cf061b79caf6705116a9
MTeguhirawan1996 <teguhirawan1996@gmail.com>
2024-05-01 21:58:18 +0700

    :construction: wip(app): meta data

---
+++ utils/constants/metadata.ts
@@ -0,0 +1,17 @@
+export const METADATA = {
+  creator: 'Teguh Irawan',
+  description: 'Personal website, portfolio, blog, programming tips of onedev',
+  keyword: 'teguh irawan, onedev',
+  authors: {
+    name: 'Teguh Irawan',
+    url: process.env.DOMAIN,
+  },
+  openGraph: {
+    url: process.env.DOMAIN,
+    siteName: 'OneDev',
+    locale: 'id-ID',
+  },
+  exTitle: '| OneDev',
+  profile:
+    'https://res.cloudinary.com/dl0neqfcw/image/upload/ar_1:1,b_rgb:ffffff,bo_0px_solid_rgb:ffffff,c_fill,g_auto,r_max,w_1000/v1696918449/Portfolio/Close_up_sczzhi.jpg',
+};
